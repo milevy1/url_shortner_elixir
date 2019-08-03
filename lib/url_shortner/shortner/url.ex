e3f29b6b3432fe6a309db470cb1daeb7b7be0213
Matt Levy <milevy1@gmail.com>
2019-08-03 14:43:10 -0600

    Adds vaildate URIs to changeset

--- lib/url_shortner/shortner/url.ex
+++ lib/url_shortner/shortner/url.ex
@@ -16,5 +16,26 @@ defmodule UrlShortner.Shortner.URL do
     |> validate_required([:short_url, :long_url])
     |> unique_constraint(:short_url)
     |> unique_constraint(:long_url)
+    |> validate_uris(:long_url)
+  end
+
+  defp validate_uris(%{changes: changes} = changeset, field) do
+    with {:ok, long_url} <- Map.fetch(changes, field),
+         parsed_url <- URI.parse(long_url),
+         {:ok, _parsed_url} <- valid_host_and_scheme?(parsed_url) do
+      changeset
+    else
+      _ -> changeset |> add_error(field, "Invalid URI")
+    end
+  end
+
+  defp valid_host_and_scheme?(%URI{host: _host, scheme: nil} = parsed_url),
+    do: {:error, parsed_url}
+
+  defp valid_host_and_scheme?(%URI{host: nil, scheme: _scheme} = parsed_url),
+    do: {:error, parsed_url}
+
+  defp valid_host_and_scheme?(%URI{host: _host, scheme: _scheme} = parsed_url) do
+    {:ok, parsed_url}
   end
 end
